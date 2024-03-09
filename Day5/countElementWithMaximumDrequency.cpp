cf1c0f1549235ec72115ca35e7bb33a588235fb0
baigfaraz <baigfaraz000@gmail.com>
2024-03-09 09:39:13 +0500

    countElementWithMaximumDrequency changing

--- Day5/countElementWithMaximumDrequency.cpp
+++ Day5/countElementWithMaximumDrequency.cpp
@@ -1,56 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <algorithm>
 using namespace std;
-
-int searchNumber(vector<int> &arr, int number)
-{
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (arr[i] == number)
-        {
-            return 1;
-        }
-    }
-    return 0;
-}
 
 void countElementWithMaximumDrequency(vector<int> &arr)
 {
     int count = 0;
-    int temp = 0;
     vector<int> mostFrequentNumber;
     for (int i = 0; i < arr.size(); i++)
     {
-
-        for (int j = 0; j < arr.size(); j++)
+        int temp = std ::count(arr.begin(), arr.end(), arr[i]);
+        if (temp > count)
         {
-            if (arr[i] == arr[j])
-            {
-                temp++;
-            }
-        }
-
-        if (temp > count || temp == count)
-        {
-            if (searchNumber(mostFrequentNumber, arr[i]) == 0)
-            {
-                mostFrequentNumber.push_back(arr[i]);
-            }
             count = temp;
-            temp = 0;
+            mostFrequentNumber.clear();
+            mostFrequentNumber.push_back(arr[i]);
+        }
+        else if (temp == count)
+        {
+            mostFrequentNumber.push_back(arr[i]);
         }
     }
-    cout << "Most Frequent Number is : ";
-    for (int i = 0; i < mostFrequentNumber.size(); i++)
-    {
-        cout << mostFrequentNumber[i] << " ";
-    }
+    cout << mostFrequentNumber.size();
 }
 
 int main()
 {
-    vector<int> arr = {1, 2, 2, 3, 1, 4};
+    vector<int> arr = {10, 12, 11, 9, 6, 19, 11};
     countElementWithMaximumDrequency(arr);
     return 0;
 }
