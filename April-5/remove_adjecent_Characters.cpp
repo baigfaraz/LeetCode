00be78bb020c2f8ffcfcf25e038a5f99196dc851
baigfaraz <baigfaraz000@gmail.com>
2024-04-05 12:01:18 +0500

    make string Good

--- April-5/remove_adjecent_Characters.cpp
+++ April-5/remove_adjecent_Characters.cpp
@@ -1,12 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <stack>
+#include <cctype>
 using namespace std;
 
-string makeGood(string s) {
-            
+bool isUpper(char c) { return c >= 'A' && c <= 'Z'; }
+
+bool isLower(char c) { return c >= 'a' && c <= 'z'; }
+
+string makeGood(string s)
+{
+    stack<char> mystack;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mystack.empty())
+            mystack.push(s[i]);
+        else
+        {
+            if (isLower(mystack.top()) && isUpper(s[i]) && tolower(mystack.top()) == tolower(s[i]))
+                mystack.pop();
+            else if (isUpper(mystack.top()) && isLower(s[i]) && tolower(mystack.top()) == tolower(s[i]))
+                mystack.pop();
+            else
+                mystack.push(s[i]);
+        }
+    }
+
+    string res = "";
+    while (!mystack.empty())
+    {
+        res = mystack.top() + res;
+        mystack.pop();
+    }
+    cout << res << endl;
+    return res;
 }
 
 int main()
 {
+    string s = "mC";
+    makeGood(s);
 
     return 0;
 }
